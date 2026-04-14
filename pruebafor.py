e1704f14e33701af1686ac0c1c6b15c3152213b2
Tenma404 <thomas.snake22@gmail.com>
2026-04-14 13:31:37 -0400

    Tercer Commit

--- pruebafor.py
+++ pruebafor.py
@@ -1,9 +1,12 @@
-# for i in range (3):
-#     print ("N" , i+1)
-
-# Repeticiones dadas por el usuario 
-
-numero=int (input (" ingrese el numero a multiplicar"))
-
-for i in range (10):
-    print ("x=  ", numero*i (i+1 ) )
+# cantidadl=0
+# Nombre=input ("ingrese su nombre")
+# for i in Nombre:
+#     print (i)
+#     cantidadl=cantidadl+1
+#     print ("la cantidad de letras es : " , cantidadl)
+nombre=input ("ingrese su nombre")
+cantvoca=0
+for i in "aeiouAEIOU":
+    print (i)
+    cantvoca+=1
+    print ("la cantidad de vocales es " , cantvoca)
