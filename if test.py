8e27eb9492673a7a63ea1175bb108de972a663a9
Tenma404 <thomas.snake22@gmail.com>
2026-04-14 12:49:08 -0400

    EJEMPLO DE FOR

---
+++ if test.py
@@ -0,0 +1,14 @@
+print (
+    '''
+1-. peras 1200
+2.- manzanas 1400
+3.- Naranjas 2000
+''' )
+select=int (input ("seleccione una fruta ") )
+if select==1:
+    print ("el total a pagar es" , 1200*1.19)
+else:
+    select==2 
+    print ("el total a pagar es ", 1400*1.19)
+    if select==3:
+        print  ("el total a pagar es  ", 2000*1.19)
---
+++ pruebafor.py
@@ -0,0 +1,9 @@
+# for i in range (3):
+#     print ("N" , i+1)
+
+# Repeticiones dadas por el usuario 
+
+numero=int (input (" ingrese el numero a multiplicar"))
+
+for i in range (10):
+    print ("x=  ", numero*i (i+1 ) )
