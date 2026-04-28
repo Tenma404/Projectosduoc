2777424d8dfb2e61617d007d9a20012d836a0152
Tenma404 <thomas.snake22@gmail.com>
2026-04-28 11:37:55 -0400

    randomly commmit 7

---
+++ randomly.py
@@ -0,0 +1,34 @@
+#uso y ejemplos de random
+
+
+# import random
+# num=random.randint(1,10)
+# print (num)
+
+# for i in range (num):
+#     print ("Hola Deny")
+
+#crea un numero random entre 1 y 100
+#Pide al usuario que adivine el numero 
+#si el usuario pone un numero mayor al generado
+# debe decir "te pasaste", en caso contrario 
+# "el numero a adivinar es mayor "
+# solo hay 5 intentos para adivinar
+
+import random
+
+print (" intente adivinar el numero")
+num=random.randint(1,100)
+intentos=5
+while intentos > 0:
+    adivina=int (input("ingrese un numero de 1 a 100:"))
+    if adivina > num:
+        print (" te pasaste, intentos restantes:", intentos-1)
+    elif adivina < num:
+        print (" el numero a adivinar es mayor , intentos restantes:", intentos-1)
+    else:
+        print ("felicidades, has adivinado el numero !!, tus intentos restantes son:", intentos)
+    intentos-=1
+if intentos == 0:
+    print (" lo siento, has agotado tus intentos, el numero era:", num)
+    
