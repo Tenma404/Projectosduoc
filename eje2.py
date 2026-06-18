c9add017f261c398158c4a441d963645635c169b
Tenma404 <thomas.snake22@gmail.com>
2026-06-18 12:48:15 -0400

    Trabajo de repaso para evaluacion n4

--- eje2.py
+++ eje2.py
@@ -1,29 +1,91 @@
-# Crear un gestor de pacientes 
-
 pacientes=[
-
-  {"Nombre": " Aquiles Baeza", "Preevision":  "Fonasa", "Temperatura": 35.6,
-   "Grave":False } 
-   
-
-
+    {"nombre": " Aquiles Baeza", "prevision": "Fonasa", 
+     "temperatura":34.6, "grave": False}
 ]
 
+'''crear al gestor de pacientes en un centro medico
+Para poner el nombre se debe validar que no este vacio 
+y ademas tenga mas de 8 caracteres
+Para la prevision de salud solo exiten 3 posibles valores
+Fonasa, Isapre, o Fodesa
+Al ingresar un paciente, se debe poner la temperatura
+Crear una funcion que valide si esta grave o no
+Para que este grave debe tener mas de 39°
+Cada atencion vale $25.000
+Los despcuentos corresponden a 
+FOnasa 54%
+Isapre 27%
+Fodesa 12,5%
 
-# Crear al gestor de pacientes en un centro medico donde
-#para poner el nombre se debe validar que no este vacio 
-#y aqdemas tenga 8 caracteres 
-#para la preevision solo existen 3 posibles valores Fonasa Isapre o Fodesa
-#al ingresar el paciente se debe ingresar la temperatura y crear funcion que determine si esta grave o no 
-# si el valor de la fiebre es superior o igual a 39 grados celcius
+'''
 
-
-
-
-
-
-
-def agregarpaciente ():
-    paciente=input("Ingrese el nombre del paciente")
-    temp=float(input("Ingrese la temperatura corporal del paciente "))
+def prevision ():
+    prev=int(input("Eliga su prevision 1 . Fonasa, 2 . Isapre, 3  Fodesa"))
+    if prev==1:
+        descuennto=25.000*54/100
+        print (f"su total a pagar es  :  {descuennto}")
+    elif prev==2:
+        descuennto=25.000*27/100
+        print (f"su total a pagar es  :  {descuennto}")
+    elif prev==3:
+        descuennto=25.000*12.5/100
+        print (f"su total a pagar es  :  {descuennto}")
+    else:
+        print ("Ingrese un valor valido y intentelo nuevamente ")
+        
     
+
+
+       
+
+
+
+def validarEstado(tempe):
+   if tempe>39:
+       return True 
+   else:
+       return False
+def mostrarPacientes():
+    if len(pacientes)==0:
+        print("No hay pacientes")
+    else:
+        c=1
+        for p in pacientes:
+            print(f"{c} .- {p}")
+            c+=1
+while True:
+    try:
+        print("1.- Ingresar paciente")
+        print("2.- Quitar paciente")
+        print("3.- Tomar Temperatura")
+        print("4.- Cobra atencion")
+        print("5.- Mostrar Pacientes")
+        print("9.- Salir")
+        op=int(input("Ingrese una opcion: "))
+        match op:
+            case 1:
+                nombre=input("Ingrese nombre: ")
+                prevision=input("Ingrese prevision: ")
+                temp=float(input("Ingrese temp: "))
+                pacientes.append({"nombre": nombre, "prevision": prevision, 
+                            "temperatura":temp, "grave": validarEstado(temp)})
+                print("Paciente agregado al listado")
+            case 2:
+                mostrarPacientes()
+                paci=int(input("Que paciente se vá?: "))
+                pacientes.pop(paci-1)
+                print("Paciente eliminado.")
+            case 3:
+                print("")
+            case 4:
+                prevision()
+                
+            case 5:
+                mostrarPacientes()
+            case 9:
+                print("Saliendo")
+                break
+            case _:
+                print("Opción inválida")
+    except Exception as e:
+        print("Error:" , e)
